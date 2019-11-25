318665813543defbffe08c914c852e6da34dd2bd
XuHaijwill <zuiwoxuanlou@163.com>
2019-11-25 14:47:53 +0800

    code commit

---
+++ workspases/demo-Spring/springmvc-test-one/src/main/java/com/test/spring/springmvc_test_one/App.java
@@ -0,0 +1,13 @@
+package com.test.spring.springmvc_test_one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
