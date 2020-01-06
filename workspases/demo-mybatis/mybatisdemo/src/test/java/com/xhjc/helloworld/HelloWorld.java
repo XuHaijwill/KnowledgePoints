e2da5f39d0185b8df114540d644538e5ddf980ec
XuHaijwill <zuiwoxuanlou@163.com>
2020-01-06 08:16:22 +0800

    code commit

---
+++ workspases/demo-mybatis/mybatisdemo/src/test/java/com/xhjc/helloworld/HelloWorld.java
@@ -0,0 +1,45 @@
+package com.xhjc.helloworld;
+
+import com.xhjc.bean.Employee;
+import com.xhjc.cach.dao.EmployeeMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @Author xhj
+ * @Description 源码查看
+ * @Date 2020/1/6 8:09
+ **/
+public class HelloWorld {
+
+    public SqlSessionFactory getSqlSessionFactory() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        return new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    @Test
+    public void test01() throws IOException {
+        // 1、获取sqlSessionFactory对象
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        // 2、获取sqlSession对象
+        SqlSession openSession = sqlSessionFactory.openSession();
+        try {
+            // 3、获取接口的实现类对象
+            //会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+            EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+            Employee employee = mapper.getEmpById(1);
+            System.out.println(mapper);
+            System.out.println(employee);
+        } finally {
+            openSession.close();
+        }
+
+    }
+}
