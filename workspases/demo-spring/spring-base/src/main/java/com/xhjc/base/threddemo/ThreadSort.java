ae820effd459075cbe40e080641a4723d3bdbcf8
XuHaijwill <zuiwoxuanlou@163.com>
2020-04-19 15:09:33 +0800

    code commit

--- workspases/demo-spring/spring-base/src/main/java/com/xhjc/base/threddemo/ThreadSort.java
+++ workspases/demo-spring/spring-base/src/main/java/com/xhjc/base/threddemo/ThreadSort.java
@@ -6,4 +6,27 @@ package com.xhjc.base.threddemo;
  * @Date 2020-04-19 14:36
  **/
 public class ThreadSort {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() ->{
+            System.out.println("thread1");
+        });
+
+        Thread thread2 = new Thread(() ->{
+            System.out.println("thread2");
+        });
+
+        Thread thread3 = new Thread(() ->{
+            System.out.println("thread3");
+        });
+
+        thread1.start();
+        thread1.join();
+        thread2.start();
+        thread2.join();
+        thread3.start();
+        thread3.join();
+    }
+
+
 }
