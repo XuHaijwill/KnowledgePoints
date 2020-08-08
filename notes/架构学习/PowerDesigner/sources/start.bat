2eef28e77f3356189c0e4f3d1f67cc344eed9bbb
unknown <zuiwoxuanlou@163.com>
2020-08-08 11:47:06 +0800

    code commit

---
+++ notes/架构学习/PowerDesigner/sources/start.bat
@@ -0,0 +1,3 @@
+Set JAVA_HOME= C:\Program Files (x86)\Java\jdk1.8.0_261
+Set CLASSPATH = D:\.m2\repository\mysql\mysql-connector-java\5.1.47\mysql-connector-java-5.1.47.jar
+PdShell16.exe
---
+++ notes/架构学习/网络拓扑图/README.md
@@ -0,0 +1,11 @@
+## 网络拓扑图绘制
+
+## 常用工具
+
+![EA1](imgs/img1.png)
+
+## viso下载安装
+
+链接：https://pan.baidu.com/s/1KAOv0nmcP9esBF-Crf64_A 
+
+提取码：09ua
